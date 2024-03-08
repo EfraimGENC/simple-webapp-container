b463bb1340f855b518a052327633a0a6edbdb179
EfraimGENC <efraim@kavimdigital.com>
2024-03-08 20:45:08 +0300

    second commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+"""
+A simple FastAPI application
+Author: Efraim GENC
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +9,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """Root endpoint"""
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+    """Say hello to the user"""
     return {"message": f"Hello {name}"}
